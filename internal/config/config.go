9daa72c51e9c3a155862d96ba29e187e4f548b73
AlexRyzhickov <makseljoinb@gmail.com>
2022-11-24 01:54:57 +0300

    change config

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,9 +3,11 @@ package config
 import "github.com/caarlos0/env/v6"
 
 type Config struct {
-	GetPort  string `env:"GET_PORT" envDefault:"8080"`
-	SetPort  string `env:"SET_PORT" envDefault:"8081"`
-	Priority uint   `env:"PRIORITY" envDefault:"1"`
+	GetPort              string `env:"GET_PORT" envDefault:"8080"`
+	SetPort              string `env:"SET_PORT" envDefault:"8081"`
+	Priority             uint   `env:"PRIORITY" envDefault:"1"`
+	ReplicasAvailability bool   `env:"REPLICAS_AVAILABILITY" envDefault:"false"`
+	ReplicasPort         string `env:"REPLICA_PORT" envDefault:"8082"`
 }
 
 func New() (*Config, error) {
